55413402f6364d05c2b58baa62c2fd139bb99d7b
martin lowe <martinlowe673@gmail.com>
2021-12-05 11:09:17 +0000

    added new info

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useSelector} from "react-redux";
+import {createSelector} from "reselect";
+import {makeSelectUsers } from "./selectors"
+import Axios from "axios"
 
+const stateSelector = createSelector(makeSelectUsers, (users) => ({
+  users
+}))
 export function HomePage(props) {
+  const { users } = useSelector(stateSelector)
+
+  const fetchUsers = async () => {
+    const response = await Axios.get("https://reqres.in/api/users").catch(
+      (err) => {
+        console.log("Err: ", err);
+      }
+    );
+  }
   return <div>hELLO</div>;
 }
